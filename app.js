ccdbfbc0bf91f67305328196a5f3fb21d6d52e1f
Matias Chirikiles <102807594+chirikiles@users.noreply.github.com>
2022-05-09 21:41:04 -0400

    actualizado con multiples extensiones

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let pronouns = ['the','our'];
 let adjs = ['great', 'big' ];
 let nouns = ['jogger','racoon'];
+let extens = ['.com','.net','.cl'];
 
 for (let pronoun of pronouns) {
     for (let adj of adjs) {
         for (let noun of nouns) {
-            console.log(pronoun + adj + noun + ".com")
+            for(let exten of extens ){           
+            console.log(pronoun + adj + noun + exten)
+        }
         }
     }
 }
